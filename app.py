a9130e10ee1c93da54e71cbe6fcc7188054a078e
tracygoznikar <tracygoznikar@gmail.com>
2021-01-18 20:11:31 -0600

    adds updated app.py file

--- app.py
+++ app.py
@@ -3,46 +3,77 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, desc
-from flask import Flask, render_template, redirect, url_for
-from flask_pymongo import PyMongo
-
-
+​
 # Import Flask
-from flask import Flask, jsonify
+# from flask import Flask, jsonify
+​
+from flask import Flask, render_template, redirect, url_for, jsonify
+​
+# app = Flask(__name__, template_folder="templates")
+​
+​
 #################################################
 # Database Setup
 #################################################
+​
 # Source:  https://stackoverflow.com/questions/39407254/how-to-set-the-primary-key-when-writing-a-pandas-dataframe-to-a-sqlite-database
-engine = create_engine("sqlite:///../meteorites4.db")
+engine = create_engine("sqlite:///meteorites4.db")
 # reflect an existing database into a new model
 base = automap_base()
+​
 # reflect the tables
 base.prepare(engine, reflect=True)
+​
 print(base.classes.keys())
 # Save references to each table
 meteorites = base.classes.meteorites
+​
 # Create an app
 app = Flask(__name__)
-
+# app = Flask(__name__, template_folder='templates')
+# app = Flask(__name__, static_folder="static")
+​
+# Define static routes
 @app.route("/")
 def homepage():
+    print("Server received request from home page...")
+    # return ("10 Biggest Meteorites<br/><br/>")
     return render_template("homepage.html")
-
-@app.route("/")
+​
+​
+@app.route("/index")
 def index():
     print("Server received request from home page...")
-    return ("10 Biggest Meteorites<br/><br/>")
-
+    # return ("10 Biggest Meteorites<br/><br/>")
+    return render_template("index.html")
+​
+​
 @app.route("/api/v1.0/bubbles")
 def bubbles():
-# Create our session (link) from Python to the DB
+    # Create our session (link) from Python to the DB
     session = Session(engine)
-    # Query meteorite size
-    meteorSize = session.query(meteorites.Mass).limit(10).all()
+​
+    results = session.query(meteorites.Mass, meteorites.name, meteorites.year,
+                            meteorites.reclat, meteorites.reclong, meteorites.GeoLocation, meteorites.id).order_by(meteorites.Mass.desc()).all()
+​
     session.close()
-    meteors = []
-    for Mass in meteorSize:
-        meteors.append(Mass)
-    return jsonify(meteors)
-if __name__=="__main__":
+​
+    mtrarray = []
+​
+    for Mass, name, year, reclat, reclong, GeoLocation, id in results:
+        mtrdict = {}
+        mtrdict["mass"] = Mass
+        mtrdict["name"] = name
+        mtrdict["year"] = year
+        mtrdict["reclat"] = reclat
+        mtrdict["reclong"] = reclong
+        mtrdict["geolocation"] = GeoLocation
+        mtrdict["id"] = id
+​
+        mtrarray.append(mtrdict)
+​
+    return jsonify(mtrarray)
+​
+​
+if __name__ == "__main__":
     app.run(debug=True)
